903e18541a148c7d17525d957ba85c18e30b2110
Zarek Vane <zarek.vane@outlook.com>
2022-07-25 00:06:31 +0800

    添加Feedback类

---
+++ js/feedback.js
@@ -0,0 +1,28 @@
+class Feedback {
+    constructor(
+        feedbackId,
+        userId,
+        name,
+        feedbackTime = "",
+        orderId = "",
+        plateDetails = [],
+        description = "",
+        result = "",
+        platePicture = "",
+        userAvatar = ""
+    ) {
+        this.feedbackId = feedbackId;
+        this.userId = userId;
+        this.name = name;
+
+        this.feedbackTime = feedbackTime;
+        this.orderId = orderId;
+        this.orderTime = orderTime;
+
+        this.plateDetails = plateDetails;
+        this.description = description;
+        this.result = result;
+        this.platePicture = platePicture;
+        this.userAvatar = userAvatar;
+    }
+}
