3adfe8a8e8ed18390dc0b49ad6501a20f5835f55
Zarek Vane <zarek.vane@outlook.com>
2022-07-24 23:11:55 +0800

    新增餐盘加个修改功能

--- js/plate.js
+++ js/plate.js
@@ -6,7 +6,8 @@ class Plate {
     }
 }
 
-var plateTable = new Table(document.getElementById("tbl_plates"), [], fFillTr);
+var gPlateTable = new Table(document.getElementById("tbl_plates"), [], fFillTr);
+var gNewPriceMap = new Map();
 
 function fFillTr(tr, plate) {
     let plateNameTd = document.createElement("td");
@@ -19,7 +20,35 @@ function fFillTr(tr, plate) {
     priceInput.min = "0.00";
     priceInput.step = "0.01";
     priceInput.value = plate.price;
+
+    priceInput.onchange = function () {
+        gNewPriceMap.set(plate.plateName, plate.price);
+    };
+
     platePriceTd.appendChild(priceInput);
+
+    let priceBtn = document.createElement("button");
+    priceBtn.innerHTML = "修改";
+
+    priceBtn.onclick = function () {
+        gAjax({
+            url: "/admin/plate/change",
+            method: "post",
+            data: JSON.stringify({
+                plate_name: plate.plateName,
+                price: plate.price,
+                picture: plate.price,
+            }),
+            success: (data) => {
+                if (data.code == 200) {
+                    alert(data.data);
+                }
+            },
+        });
+    };
+
+    platePriceTd.appendChild(priceBtn);
+
     tr.appendChild(platePriceTd);
 
     let picTd = document.createElement("td");
@@ -36,9 +65,9 @@ function getPlateList() {
         method: "get",
         success: (data) => {
             if (data.code == 200) {
-                plateTable.clearAll();
+                gPlateTable.clearAll();
                 data.data.forEach((e) => {
-                    plateTable.push(
+                    gPlateTable.push(
                         new Plate(e.plate_name, e.price, e.plate_img_url)
                     );
                 });
